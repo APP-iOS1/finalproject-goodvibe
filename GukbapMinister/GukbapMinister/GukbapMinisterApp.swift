02cfd3ff8ca370cb5aa4ef8d0ee7da59955ad4d2
MartinLeeSJ <tjr2922@gmail.com>
2023-01-16 21:06:31 +0900

    [Add]프로젝트 파일 생성

---
+++ GukbapMinister/GukbapMinister/GukbapMinisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GukbapMinisterApp.swift
+//  GukbapMinister
+//
+//  Created by Martin on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct GukbapMinisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
