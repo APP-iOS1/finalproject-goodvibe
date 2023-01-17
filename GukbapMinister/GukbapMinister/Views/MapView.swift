b5b525c6b144c71dda3f823ca305051f7b0545b5
KiTaeUk <ktw1421@gmail.com>
2023-01-17 18:02:30 +0900

    [Feat] mapView 검색, 필터 기능 구현

--- GukbapMinister/GukbapMinister/Views/MapView.swift
+++ GukbapMinister/GukbapMinister/Views/MapView.swift
@@ -10,11 +10,67 @@ import SwiftUI
 struct MapView: View {
     // 초기 좌표값 서울시청
     @State var coordination: (Double, Double) = (37.5666805, 126.9784147)
+    @State var searchGukBap : String = ""
+    @State private var showModal = false //상태
     
     var body: some View {
         
         ZStack {
             VStack {
+                
+                HStack{
+                    ZStack {
+                        VStack {
+                            HStack {
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(.secondary)
+                                    .padding(.leading, 15)
+                                TextField("국밥집 검색",text: $searchGukBap)
+                            }
+                            .frame(width: 280, height: 50)
+                            .background(Capsule().fill(Color.white))
+                            .overlay {
+                                Capsule()
+                                    .stroke(.yellow)
+                            }
+                        }
+                    }
+                    
+                    Button{
+                        //
+                    } label: {
+                        Text("확인")
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 65, height: 50)
+                    .background(.yellow)
+                    .cornerRadius(25)
+                }
+                
+                HStack{
+                    Button{
+                        self.showModal = true
+                    } label: {
+                        Text(Image(systemName: "slider.horizontal.3")).foregroundColor(.gray) + Text("필터")
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(Capsule().fill(Color.white))
+                    .overlay {
+                        Capsule()
+                            .stroke(.yellow)
+                    }
+                    .padding(.leading, 10)
+                    .sheet(isPresented: self.$showModal) {
+                        MapCategoryModalView()
+                            .presentationDetents([.medium])
+                    }
+                    
+                    Spacer()
+                    
+                }
+                
                 Button(action: {coordination = (35.1379222, 129.05562775)}) {
                     Text("부산으로 위치 이동")
                 }
