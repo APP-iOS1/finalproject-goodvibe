d810dbdf5c38c6d64fb0cbe06dbe6777b769e04e
MartinLeeSJ <tjr2922@gmail.com>
2023-01-17 22:44:20 +0900

    [Feat] 디테일뷰 완성, 댓글유닛 추가

--- GukbapMinister/GukbapMinister/Views/DetailView.swift
+++ GukbapMinister/GukbapMinister/Views/DetailView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct DetailView: View {
-    let colors: [Color] = [.yellow, .green, .red]
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     @State private var text: String = ""
+    @State private var isBookmarked: Bool = false
+    
+    let colors: [Color] = [.yellow, .green, .red]
     
     var body: some View {
         GeometryReader { geo in
@@ -19,201 +22,170 @@ struct DetailView: View {
             
             
             ScrollView {
-                VStack(alignment: .leading){
-                    
-                    VStack(alignment: .leading){
-                        Text("농민백암순대")
-                            .font(.title.bold())
-                            .padding(.bottom, 8)
-                        Text("서울 강남구 역삼로3길 20-4")
-                    }
-                    .padding(15)
-                    
-                    TabView {
-                        ForEach(Array(colors.enumerated()), id: \.offset) { index, color in
-                            
+                ZStack {
+                    Color(uiColor: .systemGray6)
+
+                    VStack(alignment: .leading, spacing: 0){
+                        
+                        //상호명 주소
+                        //Store.storeName, Store.storeAddress
+                        HStack {
+                            VStack(alignment: .leading){
+                                Text("농민백암순대")
+                                    .font(.title.bold())
+                                    .padding(.bottom, 8)
+                                Text("서울 강남구 역삼로3길 20-4")
+                            }
+                            Spacer()
+                        }
+                        .padding(15)
+                        .background(.white)
+                        
+                        //Store.images
+                        TabView {
+                            ForEach(Array(colors.enumerated()), id: \.offset) { index, color in
+                                
+                                VStack {
+                                    Text("사진\(index + 1)")
+                                        .foregroundColor(.white)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .frame(height: width * 0.8)
+                                .background(color)
+                            }
+                        }
+                        .frame(height:width * 0.8)
+                        .tabViewStyle(.page(indexDisplayMode: .always))
+                        
+                        //Store.description
+                        VStack {
+                            Text("수요미식회에서 인정한 선릉역 찐 맛집!")
+                                .lineLimit(10)
+                                .padding(.horizontal, 15)
+                            .padding(.vertical, 30)
+                            Divider()
+                        }
+                        .background(.white)
+                        
+                        
+                        //Store.menu
+                        VStack {
+                            VStack(alignment: .leading) {
+                                Text("메뉴")
+                                    .font(.title2.bold())
+                                    .padding(.bottom)
+                                
+                                HStack{
+                                    Text("국밥")
+                                    Spacer()
+                                    Text("9,000원")
+                                }
+                                .padding(.bottom, 5)
+                                
+                                HStack{
+                                    Text("술국")
+                                    Spacer()
+                                    Text("18,000원")
+                                }
+                                .padding(.bottom, 5)
+                                
+                                HStack{
+                                    Text("수육")
+                                    Spacer()
+                                    Text("32,000원")
+                                }
+                                .padding(.bottom, 5)
+                                
+                                HStack{
+                                    Text("토종순대")
+                                    Spacer()
+                                    Text("12,000원")
+                                }
+                                .padding(.bottom, 5)
+                            }
+                            .padding(15)
+                            Divider()
+                        }
+                        .background(.white)
+                        
+                        
+                        
+                        //네이버맵뷰
+                        NaverMapView(coordination: (37.503693, 127.053033))
+                            .frame(height: 260)
+                            .padding(.vertical, 15)
+                        
+                        
+                        HStack {
+                            Spacer()
                             VStack {
-                                Text("사진\(index + 1)")
-                                    .foregroundColor(.white)
-                            }
-                            .frame(maxWidth: .infinity)
-                            .frame(height: width * 0.8)
-                            .background(color)
-                        }
-                    }
-                    .frame(height:width * 0.8)
-                    .tabViewStyle(.page(indexDisplayMode: .always))
-                    .padding(.bottom)
-                    
-                    //Store.description
-                    Text("수요미식회에서 인정한 선릉역 찐 맛집!")
-                        .padding(.horizontal, 15)
-                        .padding(.bottom, 30)
-                    
-                    Divider()
-                    
-                    VStack(alignment: .leading) {
-                        Text("메뉴")
-                            .font(.title2.bold())
-                            .padding(.bottom)
-                        
-                        HStack{
-                            Text("국밥")
-                            Spacer()
-                            Text("9,000원")
-                        }
-                        .padding(.bottom, 5)
-                        
-                        HStack{
-                            Text("술국")
-                            Spacer()
-                            Text("18,000원")
-                        }
-                        .padding(.bottom, 5)
-                        
-                        HStack{
-                            Text("수육")
-                            Spacer()
-                            Text("32,000원")
-                        }
-                        .padding(.bottom, 5)
-                        
-                        HStack{
-                            Text("토종순대")
-                            Spacer()
-                            Text("12,000원")
-                        }
-                        .padding(.bottom, 5)
-                    }
-                    .padding(15)
-                    
-                    Divider()
-                    
-                    NaverMapView(coordination: (37.503693, 127.053033))
-                        .frame(height: 260)
-                        .padding(.vertical, 15)
-                    
-                    
-                        VStack{
-                            Text("테디베어님의 후기를 남겨주세요")
-                                .fontWeight(.bold)
-                                .padding(.vertical)
-                            HStack {
-                                ForEach(0..<5) { _ in
-                                    Image(systemName: "star.fill")
-                                        .foregroundColor(.yellow)
-                                }
-                            }
-                        }
-                        .padding(.vertical, 15)
-                        
-                        
-                    Group {
-                        //사진없는 댓글
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text("써니")
-                                    .font(.headline)
-                                    .padding(.bottom, 10)
-                                Spacer()
-                                Text("2023.01.17")
-                                    .font(.caption)
-                                    .foregroundColor(.gray)
+                                    Text("테디베어님의 후기를 남겨주세요")
+                                        .fontWeight(.bold)
+                                        
+                                    Spacer()
                                 
-                            }
-                            //별점
-                            HStack(spacing: 0){
-                                ForEach(0..<5) { _ in
-                                    Image(systemName: "star.fill")
-                                        .foregroundColor(.yellow)
-                                        .font(.caption)
-                                        .padding(.trailing, 2)
-                                }
-                            }
-                            .padding(.bottom, 15)
-                            
-                            Text("여기 외 않와? 꼭 가세요")
-                                .font(.footnote)
-                        }
-                        
-                        //사진있는 댓글
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text("써니")
-                                    .font(.headline)
-                                    .padding(.bottom, 10)
-                                Spacer()
-                                Text("2023.01.17")
-                                    .font(.caption)
-                                    .foregroundColor(.gray)
-                                
-                            }
-                            
-                            //별점
-                            HStack(spacing: 0){
-                                ForEach(0..<5) { _ in
-                                    Image(systemName: "star.fill")
-                                        .foregroundColor(.yellow)
-                                        .font(.caption)
-                                        .padding(.trailing, 2)
-                                }
-                            }
-                            .padding(.bottom, 15)
-                            
-                            Text("여기 외 않와? 꼭 가세요")
-                                .font(.footnote)
-                            
-                            ScrollView(.horizontal) {
-                                LazyHStack {
-                                    ForEach(0..<5) { index in
-                                        Text("\(index)")
-                                            .frame(width: 100, height: 100)
-                                            .background(.yellow)
+                                    //별 재사용 예정
+                                    HStack {
+                                        ForEach(0..<5) { _ in
+                                            Image(systemName: "star.fill")
+                                                .foregroundColor(.yellow)
+                                        }
                                     }
+                            }
+                            .padding(.vertical, 30)
+                            
+                            Spacer()
+                        }
+                        .background(.white)
+                  
+                            
+                        VStack(spacing: 0) {
+                            ForEach(0..<3) { index in
+                                let imageTest: [String] = .init(repeating: "Test", count: index)
+                                CommentUnit(nickname: "써니\(index)", date: "2023.01.17", starRate: 3.5, comment: "여기 외 않가?", images: imageTest)
+                                if index != 2 {
+                                    Divider()
                                 }
                             }
-                            .frame(height:100)
-                            
                         }
+                        .padding(.vertical, 15)
+                     
                         
-                        //사진없는 댓글
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text("써니")
-                                    .font(.headline)
-                                    .padding(.bottom, 10)
-                                Spacer()
-                                Text("2023.01.17")
-                                    .font(.caption)
-                                    .foregroundColor(.gray)
-                                
-                            }
-                            //별점
-                            HStack(spacing: 0){
-                                ForEach(0..<5) { _ in
-                                    Image(systemName: "star.fill")
-                                        .foregroundColor(.yellow)
-                                        .font(.caption)
-                                        .padding(.trailing, 2)
-                                }
-                            }
-                            .padding(.bottom, 15)
-                            
-                            Text("여기 외 않와? 꼭 가세요")
-                                .font(.footnote)
-                        }
+                        
                     }
-                    
-                    
+                    .padding(.bottom, 200)
                 }
-                .padding(.bottom, 200)
             }
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(systemName: "arrow.backward")
+                            .tint(.black)
+                    }
+                }
+                
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        isBookmarked.toggle()
+                    } label: {
+                        Image(systemName: isBookmarked ? "heart.fill" : "heart")
+                            .tint(.yellow)
+                    }
+                }
+            }
+            
         }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        NavigationStack {
+            DetailView()
+        }
     }
 }
