9cd1ee61384cfe0b3c9503eb5992b979c8824932
MartinLeeSJ <tjr2922@gmail.com>
2023-01-17 16:19:04 +0900

    [Chore]브랜치 생성전 커밋

--- GukbapMinister/GukbapMinister/ContentView.swift
+++ GukbapMinister/GukbapMinister/ContentView.swift
@@ -13,28 +13,33 @@ struct ContentView: View {
         TabView(selection: $tabSelection) {
             ExploreView()
                 .tabItem {
+                    Image(systemName: "binoculars.fill")
                     Text("둘러보기")
                 }
                 .tag(0)
             
             MapView()
                 .tabItem {
+                    Image(systemName: "binoculars.fill")
                     Text("지도")
                 }
                 .tag(1)
             
             CollectionView()
                 .tabItem {
+                    Image(systemName: "binoculars.fill")
                     Text("내가 찜한 곳")
                 }
                 .tag(2)
             
             MyPageView()
                 .tabItem {
+                    Image(systemName: "binoculars.fill")
                     Text("마이페이지")
                 }
                 .tag(3)
         }
+        .tint(Color.systemYellow)
         .padding()
     }
 }
