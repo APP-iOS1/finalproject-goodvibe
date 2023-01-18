400923dcd89d6a219aeb29fa191639fbe547834d
이원형 <67450169+whl0526@users.noreply.github.com>
2023-01-18 17:03:31 +0900

    [Chore] 파일 경로 수정

--- GukbapMinister/GukbapMinister/MainTabView.swift
+++ GukbapMinister/GukbapMinister/MainTabView.swift
@@ -29,7 +29,7 @@ struct MainTabView: View {
                 }
                 .tag(2)
             
-            CreateReviewView()
+            MyPageView()
                 .tabItem {
                     Text("마이페이지")
                 }
