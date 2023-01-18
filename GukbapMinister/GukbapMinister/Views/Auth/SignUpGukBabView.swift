3dce4dc18452c494336644bc21b2fd910bb1650b
kaito <sudoswift@gmail.com>
2023-01-18 09:41:23 +0900

    [Add] add signupInfoView, signupgukbapview

---
+++ GukbapMinister/GukbapMinister/Views/Auth/SignUpGukBabView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpGukBabView.swift
+//  GukbapMinister
+//
+//  Created by ishio on 2023/01/18.
+//
+
+import SwiftUI
+
+struct SignUpGukBabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpGukBabView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpGukBabView()
+    }
+}
---
+++ GukbapMinister/GukbapMinister/Views/Auth/SignUpInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpInfoView.swift
+//  GukbapMinister
+//
+//  Created by ishio on 2023/01/18.
+//
+
+import SwiftUI
+
+struct SignUpInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpInfoView()
+    }
+}
