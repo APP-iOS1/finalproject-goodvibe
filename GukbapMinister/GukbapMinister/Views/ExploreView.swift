1a29f55842212e4c11a4b92d43a08b78af9fa444
이원형 <67450169+whl0526@users.noreply.github.com>
2023-01-30 17:25:26 +0900

    [Fix] storeViewModel var store 변경

--- GukbapMinister/GukbapMinister/Views/ExploreView.swift
+++ GukbapMinister/GukbapMinister/Views/ExploreView.swift
@@ -13,11 +13,11 @@ struct ExploreView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false, content: {
             VStack{
-                ForEach(storesViewModel.stores){ store in
+                ForEach(storesViewModel.stores){ store2 in
                     NavigationLink{
                         DetailView()
                     } label:{
-                        StoreView(store: store)
+                        StoreView(store2: store2)
                     }
                     
                     
@@ -28,14 +28,14 @@ struct ExploreView: View {
 }
 
 struct StoreView: View{
-    var store: Store
+    var store2: Store
     
     var body: some View{
         VStack(spacing: 15) {
             HStack{
-                Text("\(store.storeName)")
-                Text("\(store.storeAddress)")
-                Text("\(store.description)")
+                Text("\(store2.storeName)")
+                Text("\(store2.storeAddress)")
+                Text("\(store2.description)")
             }
         }
     }
