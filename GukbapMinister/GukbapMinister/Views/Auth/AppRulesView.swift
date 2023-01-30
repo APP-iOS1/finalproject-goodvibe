0423c5ba673bf46147ca13247d6d106bd499fd0a
kaito <sudoswift@gmail.com>
2023-01-30 15:12:41 +0900

    [Add] .toolbar Background 에 색 넣기

--- GukbapMinister/GukbapMinister/Views/Auth/AppRulesView.swift
+++ GukbapMinister/GukbapMinister/Views/Auth/AppRulesView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct AppRulesView: View {
+    @Environment(\.dismiss) var returnSignUpView
+    
     var body: some View {
-        Text("App Rules View")
-    }
+        NavigationStack {
+            VStack {
+                Text("App Rules View")
+            } //VStack
+            .toolbar{
+                ToolbarItem(placement: .cancellationAction) {
+                    Button {
+                        returnSignUpView()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.black)
+                    }
+                }
+            }//toolbar
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.yellow, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+        }//NavigationStack
+    }//var body
 }
 
 struct AppRulesView_Previews: PreviewProvider {
--- GukbapMinister/GukbapMinister/Views/Auth/UserRulesView.swift
+++ GukbapMinister/GukbapMinister/Views/Auth/UserRulesView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct UserRulesView: View {
+    @Environment(\.dismiss) var returnSignUpView
+    
     var body: some View {
-        Text("User Rules View")
-    }
+        NavigationStack {
+            VStack {
+                Text("User Rules View")
+            } //VStack
+            .toolbar{
+                ToolbarItem(placement: .cancellationAction) {
+                    Button {
+                        returnSignUpView()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.black)
+                    }
+                }
+            }//toolbar
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.yellow, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+        }//NavigationStack
+    }//var body
 }
 
 struct UserRulesView_Previews: PreviewProvider {
     static var previews: some View {
-        UserRulesView()
+        NavigationStack{
+            UserRulesView()
+        }
     }
 }
