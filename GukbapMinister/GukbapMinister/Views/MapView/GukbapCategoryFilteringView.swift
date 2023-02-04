93e5912447b4993e69849eab07ce70c675ffdb9c
MartinLeeSJ <tjr2922@gmail.com>
2023-02-04 23:09:19 +0900

    [Add, Fix] Gukbaps Enum image, uiImage 프로퍼티 추가, 서치바 폭 조절

--- GukbapMinister/GukbapMinister/Views/MapView/GukbapCategoryFilteringView.swift
+++ GukbapMinister/GukbapMinister/Views/MapView/GukbapCategoryFilteringView.swift
@@ -7,22 +7,6 @@
 
 import SwiftUI
 
-//enum Gukbaps: String, CaseIterable {
-//    case 순대국밥 = "순대국밥"
-//    case 돼지국밥
-//    case 내장탕
-//    case 선지국
-//    case 소머리국밥
-//    case 뼈해장국
-//    case 굴국밥
-//    case 콩나물국밥
-//    case 설렁탕
-//    case 평양온반
-//    case 시레기국밥
-//}
-
-
-
 struct GukbapCategoryFilteringView: View {
     enum Mode {
         case map, myPage
