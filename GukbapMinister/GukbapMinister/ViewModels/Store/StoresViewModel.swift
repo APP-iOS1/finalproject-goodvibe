f786053a358a94a5c839f900536bf8092402ccd5
KiTaeUk <ktw1421@gmail.com>
2023-02-07 17:03:31 +0900

    [Feat] 둘러보기View UI수정, 내가 찜한 곳 UI 수정

--- GukbapMinister/GukbapMinister/ViewModels/Store/StoresViewModel.swift
+++ GukbapMinister/GukbapMinister/ViewModels/Store/StoresViewModel.swift
@@ -101,6 +101,8 @@ final class StoresViewModel: ObservableObject {
     }
     
     
+    
+    
     // MARK: - Storage에서 이미지 다운로드
 //    private func fetchImages(storeId: String, imageName: String) {
 //        let ref = storage.reference().child("storeImages/\(storeId)/\(imageName)")
